3835fe92f8bffc2801fadbcfa69ce4de262bedc9
Samyak Haribhakta <110559769+Sivernial@users.noreply.github.com>
2025-02-04 11:00:25 +0530

    MONGO uri

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
-
+import { MONGODB_URI } from "./config";
 mongoose
-  .connect(
-    "mongodb+srv://Sivernial:14Samyak_06@database1.hbedk.mongodb.net/"
-    // mongodb+srv://Sivernial:14Samyak_06@database1.hbedk.mongodb.net/
-  )
+  .connect("MONGODB_URI")
   .then(() => {
     console.log("Connected to MongoDB");
   })
